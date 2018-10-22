6fdd72702da52b646159287dce7f4109daa0f0a6
John Payne <payj1_16@uni.worc.ac.uk>
2018-10-22 19:20:56 +0100

    AnnoyingUpdates - Nothing important
    Unknown how to not update all of these files. N

--- lifecycle/platforms/android/app/src/main/assets/www/js/index.js
+++ lifecycle/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,12 +1,18 @@
 var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
+/*var key = “macbook”;
+var value = “green”;
+var value2 = window.localStorage.getItem(#macbook);*/
+
 
 document.addEventListener("deviceready", onDeviceReady, false); //Prepares device
+/*window.localStorage.setItem( "macbook", "green" );*/
 
 
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
+	/*$("#macbook").text("Colour: " + launched_count);*/
 	$("#resumed").text("Application paused: " + paused_count);
 	$("#paused").text("Application resumed: " + resumed_count);
 }
@@ -15,13 +21,6 @@ function updateDisplay() {
 // device APIs are available
 //
     function onDeviceReady() {
-
-/*    var key = “macbook”;
-    var value = “green”;
-
-    window.localStorage.setItem( 'macbook', 'green' );
-
-    var value2 = window.localStorage.getItem(#keys);*/
 
 	document.addEventListener("resume", onResume, false); //Creates event for alert message
 	document.addEventListener("pause", onPause, false); //Creates event for alert message
