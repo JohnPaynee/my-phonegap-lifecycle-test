b90f21c51fc2a011f6c7d95bbb62315d70a59be5
John Payne <payj1_16@uni.worc.ac.uk>
2018-10-22 18:48:10 +0100

    Testing 01
    Block commenting code in order to restore the application

--- lifecycle/platforms/android/app/src/main/assets/www/js/index.js
+++ lifecycle/platforms/android/app/src/main/assets/www/js/index.js
@@ -3,8 +3,8 @@ var resumed_count = 0;
 var launched_count = 0;
 
 document.addEventListener("deviceready", onDeviceReady, false); //Prepares device
-		
-	
+
+
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
@@ -16,36 +16,36 @@ function updateDisplay() {
 //
     function onDeviceReady() {
 
-    var key = “macbook”;
+/*    var key = “macbook”;
     var value = “green”;
 
     window.localStorage.setItem( 'macbook', 'green' );
 
-    var value2 = window.localStorage.getItem(#keys);
-	
+    var value2 = window.localStorage.getItem(#keys);*/
+
 	document.addEventListener("resume", onResume, false); //Creates event for alert message
 	document.addEventListener("pause", onPause, false); //Creates event for alert message
-	
+
 	launched_count++;
 	updateDisplay();
-	    
+
 	alert("device ready");
     }
 
 
     function onPause() {
-	
+
 	paused_count++;
 	updateDisplay();
-	    
+
 	alert("pause");
     }
-	
+
 
     function onResume() {
-		
+
 	resumed_count++;
 	updateDisplay();
-	    
+
 	alert("resume");
     }
